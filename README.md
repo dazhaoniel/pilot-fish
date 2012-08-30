96174e8917a75c1a0cf773f85832a40115134bd4
dazhaoniel <danielantoiny@gmail.com>
2012-08-29 22:39:17 -0400

    post formats done

--- README.md
+++ README.md
@@ -7,5 +7,5 @@ Current Version: 0.3.2
 
 To Do List:
 
-1. display page children
+1. move sidebar-footer from footer.php to sidebar-footer.php
 2. theme options
--- single.php
+++ single.php
@@ -4,61 +4,26 @@
  *
  * @file           single.php
  * @package        Pilot Fish
- * @filesource     wp-content/themes/pilotfish/single.php
+ * @filesource     wp-content/themes/pilot-fish/single.php
  * @since          Pilot Fish 0.1
  */
 get_header(); ?>
-        <div id="post" class="row span8" role="main">
-        
-<?php if (have_posts()) : ?>
-
-		<?php while (have_posts()) : the_post(); ?> 
-          
-            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-		<header><h1><?php the_title(); ?></h1></header> <!-- Post Title -->
-                <div class="post-meta">
-                <?php pilotfish_entry_meta(); ?>
-		<?php if ( comments_open() ) : ?>
-                        <span class="comments-link">
-                        <span class="mdash">&mdash;</span>
-                    <?php comments_popup_link(__('No Response &darr;', 'pilotfish'), __('One Response &darr;', 'pilotfish'), __('% Responses &darr;', 'pilotfish')); ?>
-                        </span>
-                    <?php endif; ?> 
-                </div><!-- end of .post-meta -->
-                <div class="post-entry">
-                    <?php the_content(__('Continue Reading &rarr;', 'pilotfish')); ?>
-                    
-                    <?php if ( get_the_author_meta('description') != '' ) : ?>
-                    
-                    <div id="author-meta">
-                    <?php if (function_exists('get_avatar')) { echo get_avatar( get_the_author_meta('email'), '80' ); }?>
-                        <div class="about-author"><?php _e('About','pilotfish'); ?> <?php the_author_posts_link(); ?></div>
-                        <p><?php the_author_meta('description') ?></p>
-                    </div><!-- end of #author-meta -->
-                    
-                    <?php endif; // no description, no author's meta ?>
-                    
-                    <?php wp_link_pages(array('before' => '<div class="pagination">' . __('Pages:', 'pilotfish'), 'after' => '</div>')); ?>
-                </div><!-- end of .post-entry -->
-                
-                <footer class="post-data">  
-			<?php the_tags(__('TAGS:', 'pilotfish') . ' ', ', ', '<br />'); ?>
-			<?php printf(__('FILED UNDER: %s', 'pilotfish'), get_the_category_list(', ')); ?>
-                </footer><!-- end of .post-data -->             
-
-            <div class="post-edit"><?php edit_post_link(__('Edit', 'pilotfish')); ?></div>             
-            </article><!-- end of #post-<?php the_ID(); ?> -->
-            
-			<?php comments_template( '', true ); ?>
-            
-        <?php endwhile; ?> 
+	<div id="post" class="row span8" role="main">
+		<?php if (have_posts()) : ?>
+			<?php while ( have_posts() ) : the_post(); 
+				if( !get_post_format() ) {
+					get_template_part( 'content', 'standard' );
+				} else {
+					get_template_part( 'content', get_post_format() );
+				}
+				comments_template( '', true ); ?>
+			<?php endwhile; ?> 
 
 	<nav id="post-nav" class="pager">
 	<span class="previous"><?php previous_post_link( '%link', __( '<span class="meta-nav">&larr;</span> previous post', 'pilotfish' ) ); ?></span>
 	<span class="next"><?php next_post_link( '%link', __( 'next post <span class="meta-nav">&rarr;</span>', 'pilotfish' ) ); ?></span>
 	</nav>
-<?php endif; ?>  
+		<?php endif; ?>  
         </div><!-- end of #post -->
-
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
